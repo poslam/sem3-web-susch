e3569f8992c4d350ef21a5f5b69d8e4849a237cc
poslam <kirden04@gmail.com>
2023-11-27 14:20:43 +1000

    update packages

--- api/config.py
+++ api/config.py
@@ -10,10 +10,4 @@ DB_NAME = os.environ.get("DB_NAME")
 DB_USER = os.environ.get("DB_USER")
 DB_PASS = os.environ.get("DB_PASS")
 
-SECRET_AUTH = os.environ.get("SECRET_AUTH")
 AUTH_TOKEN_LIFE = os.environ.get("AUTH_TOKEN_LIFE")
-
-RT_SECRET = os.environ.get("RT_SECRET")
-REFRESH_TOKEN_LIFE = os.environ.get("REFRESH_TOKEN_LIFE")
-
-ALGORITHM = os.environ.get("ALGORITHM")
