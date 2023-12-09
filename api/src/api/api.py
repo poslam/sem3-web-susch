dc167bae53e91a75f8b0ef4650eb5e12367c22ee
xYarvinx <otebator@gmail.com>
2023-12-09 22:50:01 +1000

    session2
    sdasd

--- api/src/api/api.py
+++ api/src/api/api.py
@@ -6,13 +6,14 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from src.api.auth import auth_router
 from src.api.office import office_router
 from src.api.user import user_router
+from src.api.flight import flight_router
 
 api_router = APIRouter()
 
 api_router.include_router(auth_router, prefix="/auth")
 api_router.include_router(user_router, prefix="/user")
 api_router.include_router(office_router, prefix="/office")
-
+api_router.include_router(flight_router, prefix="/flight")
 
 @api_router.get("/serverStatus")
 async def server_status(session: AsyncSession = Depends(get_session)):
